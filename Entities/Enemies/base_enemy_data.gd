355ddac8308978553e5f09226b38440730ac8c6f
UnscriptedGames <support@playunscripted.games>
2025-07-29 17:17:15 +0200

    Create base enemy scene and data resource
    - Use Area2D as root. (Complete)
    - Add AnimatedSprite2D and CollisionShape2D. (Complete)
    - Add TextureProgressBar as health bar. (Complete)
    - Add exported variant array and random selection logic. (Complete)
    - Store enemy stats and variants in a resource file. (Complete)

---
+++ Entities/Enemies/base_enemy_data.gd
@@ -0,0 +1,9 @@
+## Data resource for enemy stats
+
+class_name EnemyData
+extends Resource
+
+@export var max_health: int = 10
+@export var speed: float = 60.0
+@export var reward: int = 1
+@export var variants: Array[String] = ["variant_1"]
